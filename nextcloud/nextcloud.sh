dd76770acb4ef66d52b2b9ed921f9ed62b9f8614
Club77Admin <club77@club77.org>
2025-03-26 16:53:33 +0800

    Create nextcloud.sh

---
+++ nextcloud/nextcloud.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+docker run \
+--init \
+--sig-proxy=false \
+--name nextcloud-aio-mastercontainer \
+--restart always \
+--publish 8080:8080 \
+--env APACHE_PORT=11000 \
+--env APACHE_IP_BINDING=0.0.0.0 \
+--env APACHE_ADDITIONAL_NETWORK="" \
+--env SKIP_DOMAIN_VALIDATION=false \
+--env NEXTCLOUD_DATADIR="/mnt/volume-sin-1/nextcloud" \
+--volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
+--volume /var/run/docker.sock:/var/run/docker.sock:ro \
+nextcloud/all-in-one:latest
