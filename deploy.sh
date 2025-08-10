b1668a05efa477b0e885d6773e4cec03d4a17cd9
Club77Admin <club77@club77.org>
2025-08-10 10:34:06 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,14 +8,6 @@ sudo apt dist-upgrade
 sudo sed -i 's/^/# /' /etc/hostname
 sudo sed -i '1 i\oracle.club77.org' /etc/hostname
 
-# Firewall
-sudo apt install ufw
-sudo ufw allow OpenSSH
-echo "Ready to enable firewall, okay to proceed?"
-echo "--- waiting for key press ---"
-read -n 1 -s
-sudo ufw enable
-
 # Github
 ssh-keygen -t ed25519 -C "club77@club77.org" -P -q
 echo "Public key generated:"
@@ -27,17 +19,3 @@ ssh-keyscan github.com >> ~/.ssh/known_hosts
 git clone git@github.com:Club77Admin/Club77Configs.git
 
 
-
-sudo cp Club77Configs/ufw/Apache /etc/ufw/applications.d/
-sudo ufw allow "Apache Full"
-
-
-
-sudo cp Club77Configs/ufw/Dovecot /etc/ufw/applications.d/
-sudo ufw allow "Dovecot Secure IMAP"
-
-
-
-sudo cp Club77Configs/ufw/SMTP /etc/ufw/applications.d/
-sudo ufw allow "SMTP"
-sudo ufw allow "SMTP Secure"
